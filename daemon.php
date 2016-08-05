e4e9847a09d2e5d123535b6c58aef2266abd4135
adoc <yibo520@gmail.com>
2016-08-05 11:28:04 +0800

    修复一堆bug，优化提示

--- daemon.php
+++ daemon.php
@@ -1,8 +1,8 @@
 <?php
-include_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . "vendor/autoload.php";
+include_once dirname(__FILE__) . DIRECTORY_SEPARATOR . "vendor/autoload.php";
 $iniPath = "/etc/pat_daemon.conf";
 if (!file_exists($iniPath)) {
-    $iniPath = dirname(__FILE__) . DIRECTORY_SEPARATOR . "pat_daemon.json";
+    $iniPath = dirname(__FILE__) . DIRECTORY_SEPARATOR . "pat_daemon.conf";
 }
 $scheduler = new \Adocwang\Pat\PhpAsyncTaskScheduler($iniPath);
 $scheduler->main($argv);
